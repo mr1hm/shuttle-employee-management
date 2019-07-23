e18cf7fe630f1eb475f420cf7658db63bb632f9a
Juliann <perrigoj@bellsouth.net>
2019-07-23 22:17:20 +0000

    add host to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
       '/api': {
         target: 'http://localhost',
         headers: {
-          Host: 'tbd'
+          Host: 'anteater-express.localhost'
         }
       }
     }
