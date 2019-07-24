4cbe07cfeecabd79c26de0385746ecb589fe62e2
Stephanie B <sburcombe@gmail.com>
2019-07-24 05:31:55 +0000

    created ShiftsWeek Component skeleton

---
+++ client/components/shifts-week.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ShiftsWeek extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>View Month</div>
+      <div>Week Shifts View</div>
+      <div>View Day</div>
+    );
+  };
+}
+
+export default ShiftsWeek;
