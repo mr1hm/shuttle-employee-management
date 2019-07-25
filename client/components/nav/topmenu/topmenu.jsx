07f91f02112773b2cdda035197fcfe7b65c82c75
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-07-25 05:58:40 -0700

    added a css loader, and stylesheet for top nav

--- client/components/nav/topmenu/topmenu.jsx
+++ client/components/nav/topmenu/topmenu.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './topmenu.css';
 
 
 const TopMenu = (props)=> (
-    <nav>
+    <nav className="topnav">
         <ul>
             <li>
                 <Link to="/">Login</Link>
