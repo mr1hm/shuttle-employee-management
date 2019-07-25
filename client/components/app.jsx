e5eed165f7fcec7ec3419ae0752e33ea5cc787bf
Juliann <perrigoj@bellsouth.net>
2019-07-25 04:10:21 +0000

    menu for welcome and login pages (no styling) routing between two pages using a function for login and component for welcome page.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import Welcome from './welcome';
 
 
  class App extends React.Component {
   constructor(props){
     super(props);
+  }
+
+  loginView() {
+    return (
+          <div className="container" style={{ top: "40%", left: "20%", position: "absolute" }}>
+            <div className="row" style={{ display: "inline", transform: "translate(-50%, -50%)"}} > 
+            This will be where Login, Welcome, and Shift will render</div>
+          </div>
+    );
   }
 
 
