571b0700186a1fd7bd714d7dd83d31690777cb4e
Stephanie B <sburcombe@gmail.com>
2019-07-25 22:01:58 +0000

    created month directory created shiftsMonth component skeleton crated shifts-month.jsx created day directory created shiftsDay component skeleton created shifts-day.jsx

---
+++ client/components/shifts/day/shifts-day.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ShiftsDay extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+          <div>View Day</div>
+    );
+  };
+}
+
+export default ShiftsDay;
