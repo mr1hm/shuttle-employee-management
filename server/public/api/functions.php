0b9f294813d3b274114fc0a1e9e90c055748ebe4
Stephanie B <sburcombe@gmail.com>
2019-07-28 18:43:33 +0000

    added handle CORS function to functions.php file

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -11,5 +11,15 @@ if (!function_exists('handleError')) {
     print($json_output);
   }
 }
+if (!function_exists('handleCors')) {
+  function handleCors()
+  {
+    header("Access-Control-Allow-Origin: *");
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+      header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+      exit();
+    }
+  }
+}
 
 ?>
