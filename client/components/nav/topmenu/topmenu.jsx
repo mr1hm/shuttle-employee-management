7e82e57957e9e107aef8b38557e3770ce8759a4e
Juliann <perrigoj@bellsouth.net>
2019-07-30 00:35:59 +0000

    moved hamburger menu to dev, routing working, does not do anything when you click outside the menu (restricted entry), heighlights when you click on an iitem

--- client/components/nav/topmenu/topmenu.jsx
+++ client/components/nav/topmenu/topmenu.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './topmenu.css';
+import HamburgerMenu from './hamburger-menu';
 
 
-const TopMenu = (props)=> (
+const TopMenu = (props)=> {
+    return (
     <nav className="topnav">
-        <ul>
-            <li>
+        <div>
+            {/* <li>
                 <Link to="/">Login</Link>
             </li>
             <li>
                 <Link to="/welcome/">Go to Welcome Page</Link>
-            </li>
-            <li>
-                <Link to="/shifts/week">Shifts (Week)</Link>
-            </li>
-        </ul>
+            </li> */}
+            <HamburgerMenu>
+                <div className="dropdown-item"><Link className="d-block text-center" to="/myinfo">MyInfo</Link></div>
+                <div className="dropdown-item"><Link className="d-block text-center" to="/shifts/day/shifts-day">Day</Link></div>
+                <div className="dropdown-item"><Link className="d-block text-center" to="/shifts/week/shifts-week">Week</Link></div>
+                <div className="dropdown-item"><Link className="d-block text-center" to="/shifts/month/shifts-month">Month</Link></div>
+                <div className="dropdown-item"><Link className="d-block text-center" to="/shifts/available/shifts-available">Available</Link></div>
+            </HamburgerMenu>
+        </div>
     </nav>
-)
 
+    )
+}
 export default TopMenu;
--- client/components/shifts/month/shifts-month.jsx
+++ client/components/shifts/month/shifts-month.jsx
@@ -6,7 +6,9 @@ class ShiftsMonth extends React.Component {
   }
   render() {
     return (
-          <div>Month View</div>
+      <div className="container" style={{ top: "40%", left: "40%", position: "absolute" }}>
+        <div className="row d-inline" style={{ transform: "translate(-50%, -50%)" }} >My Shifts - MONTH</div>
+      </div>
     );
   };
 }
