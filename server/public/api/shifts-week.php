c2fdc48c7b2af7f8bc299e60d9fd56cde59de7c0
Stephanie B <sburcombe@gmail.com>
2019-08-01 20:32:10 +0000

    added exception handler to the shifts-week.php file

--- server/public/api/shifts-week.php
+++ server/public/api/shifts-week.php
@@ -1,5 +1,10 @@
 <?php
  require_once('functions.php');
+ set_exception_handler('error_handler');
+
+
+
+
  $output = file_get_contents('dummy-data/dummy-data-shifts-week.json');
   print($output);
 ?>
