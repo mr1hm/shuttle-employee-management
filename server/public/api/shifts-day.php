052fd188679785401b9096cfe8b0cdd180f9f3a5
jbg1020 <jbg1020@gmail.com>
2019-08-01 17:57:53 -0700

    working on 2nd level php endpoint, editied php file, nothing else yet

--- server/public/api/shifts-day.php
+++ server/public/api/shifts-day.php
@@ -1,4 +1,12 @@
 <?php
  require_once('functions.php');
- $output = file_get_contents('dummy-data/dummy-data-shifts-day.json');
-  print($output);
+
+ $conn = mysqli_connect('localhost', 'root', 'root', 'anteaterExpress', 3306)
+ if ($conn) {
+     throw new Exception("Error Message: ", mysqli_connect_error('error_handler'));
+ }
+
+//  $output = file_get_contents('dummy-data/dummy-data-shifts-day.json');
+//   print($output);
+
+?>
