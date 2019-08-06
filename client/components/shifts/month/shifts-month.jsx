dc723bcf40bcf7c2983ab421f37063d264c3d305
Richard Yun <richaryun84@gmail.com>
2019-08-06 10:35:30 -0700

    Refactored calendar generating method and simplified code utilizing new Date objects and setDate methods. Retrieved the unix values for the first Sunday and last Saturday of the calendar in view. Sent the unix date ranges as queries via fetch. Modified the shifts-month.php file to use the date ranges as limits within the query. Added more validation and sanitization of query variables. Modified the fetch method in shifts-month.jsx to accept query and id variables. Added a catch for errors in the fetch method. Modified the webpack.config.js file to address the 504 error. Renamed the calendar day component file to day-of-month-component.jsx and the component name to DayOfMonth to match Seong's work.

--- client/components/shifts/month/shifts-month.jsx
+++ client/components/shifts/month/shifts-month.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import TopMenuShift from '../../topmenu/topmenu-shift';
+import DayOfMonth from './day-of-month-component';
 export default class ShiftsMonth extends React.Component {
   constructor(props) {
     super(props);
     this.generateCalendarPage = this.generateCalendarPage.bind(this);
-    this.query = "";
+    this.displayCalendarPage = this.displayCalendarPage.bind(this);
+    this.query = ``;
+    this.id = `&id=1`;
     this.state = {
       scheduledHoursForCurrentMonth: []
     }
   }
-  fetchCallMethod(query, methodToUse) {
-    fetch('/api/shifts-month.php?' + query, {method: methodToUse})
+  fetchCallMethod(query, id, methodToUse) {
+    fetch(`/api/shifts-month.php` + query + id, {method: methodToUse})
       .then(res => {return res.json()})
       .then(jsonRes => {this.setState({
         scheduledHoursForCurrentMonth: jsonRes
-      })})
+      }), console.log("state:", this.state)})
+      .catch(error => {throw(error)})
   }
   componentDidMount(){
-    this.fetchCallMethod('/api/shifts-month.php', 'GET');
+    this.fetchCallMethod(this.query, this.id, 'GET');
   }
   generateCalendarPage(unixTimeStamp) {
-    var today = new Date(unixTimeStamp);
-    var firstDayOfMonth = new Date(today);
+    var selectedDate = new Date(unixTimeStamp);
+    var firstDayOfMonth = new Date(selectedDate);
     firstDayOfMonth.setDate(1);
   
     var calendarPage = [firstDayOfMonth];
@@ -38,7 +42,6 @@ export default class ShiftsMonth extends React.Component {
       calendarPage.push(nextDayOfMonth);
       nextDayOfMonth = new Date(nextDayOfMonth);
       nextDayOfMonth.setDate(nextDayOfMonth.getDate() + 1);
-      console.log("Next day of month", nextDayOfMonth.toDateString());
     }
   
     var dayOfNextMonth = new Date(nextDayOfMonth);
@@ -48,17 +51,27 @@ export default class ShiftsMonth extends React.Component {
       dayOfNextMonth = new Date(dayOfNextMonth);
       dayOfNextMonth.setDate(dayOfNextMonth.getDate() + 1);
     }
-    
     const unixCalendarStartRange = new Date(calendarPage[0].toDateString()).getTime();
     const unixCalendarEndRange = new Date(calendarPage[calendarPage.length - 1].toDateString()).getTime();
-    this.query = ``
+    this.query = `?unixstart=${unixCalendarStartRange}&unixend=${unixCalendarEndRange}`;
     return calendarPage;
   }
+  displayCalendarPage() {
+    var monthDivArray=[];
+    var calendarPage = this.generateCalendarPage(this.props.match.params.date);
+    for(var dayOfCalendar=0; dayOfCalendar < calendarPage.length; dayOfCalendar++){
+      monthDivArray.push(<DayOfMonth dayIndex={calendarPage[dayOfCalendar].getDate()}/>)
+    }
+    return monthDivArray
+  }
   render() {
+    if (this.props.match.params.date === undefined) {
+      this.props.match.params.date = this.props.defaultDate;
+    }
     return (
       <div>
         <TopMenuShift title="MONTH" page='month' date={this.props.match.params.date}/>
-        {this.generateCalendarPage()}
+        {this.displayCalendarPage()}
       </div>
     )
   }
--- server/public/api/shifts-month.php
+++ server/public/api/shifts-month.php
@@ -3,14 +3,23 @@ require_once('functions.php');
 set_exception_handler('error_handler');
 require_once('db_connection.php');
 
-$ownerID = 1;
-$ownerID = intval($ownerID);
+$lowerDateRange = intval($_GET['unixstart']);
+$upperDateRange = intval($_GET['unixend']);
 
-$query = "SELECT `startTime`, `endTime`, `shiftDate` FROM `shift` WHERE `ownerID` = $ownerID";
+if (!empty($_GET['id'])) {
+    $ownerID = intval($_GET['id']);
+} else throw new Exception('need id for query');
 
-if (!$ownerID) {
-    throw new Exception('need id for query');
-}
+if (!empty($_GET['unixstart'])) {
+    $lowerDateRange = intval($_GET['unixstart']);
+} else throw new Exception('need lower limit date range for query');
+
+if (!empty($_GET['unixend'])) {
+    $upperDateRange = intval($_GET['unixend']);
+} else throw new Exception('need upper limit date range for query');
+
+$query = "SELECT * FROM `shift` WHERE `ownerID`= {$ownerID} AND (`shiftDate` >= {$lowerDateRange} AND `shiftDate` <={$upperDateRange})
+            ORDER BY `shiftDate` ASC";
 
 $result = mysqli_query($conn, $query);
 
