22401054bf88ac543149eb5e72ae1985ffb94df3
Richard Yun <richaryun84@gmail.com>
2019-08-07 10:23:59 -0700

    Cleaned up month component and set up for adding icons.

--- client/components/shifts/month/day-of-month-component.jsx
+++ client/components/shifts/month/day-of-month-component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+
 class DayOfMonth extends React.Component {
   render() {
-return (
-      <div>{this.props.dayIndex}</div>
+    return (
+      <div class={this.props.today}>
+        {this.props.dayIndex}
+        <div class={this.props.shifts}></div>
+      </div>
     );
-} 
+  } 
 }
+
 export default DayOfMonth;
