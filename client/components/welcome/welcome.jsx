4198117bb2ca555c7cf33ea0503d0c0a460b1359
dseong0224 <dseong0224@gmail.com>
2019-08-09 04:50:09 +0000

    final version with welcome-page

--- client/components/welcome/welcome.jsx
+++ client/components/welcome/welcome.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import TopMenuGeneral from '../topmenu/topmenu-general';
-
+import './welcome.css';
+​
 class Welcome extends React.Component {
   constructor(props) {
     super(props);
   }
-
+​
   render() {
     return (
-      <React.Fragment >
+      <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg">
         <TopMenuGeneral title="WELCOME" />
-        <div className="container" style={{ top: "40%", left: "40%", position: "absolute" }}>
-          <div className="row d-inline" style={{ transform: "translate(-50%, -50%)" }} >Welcome</div>
+        <div class="col-md-5 p-lg-5 mx-auto my-5 bg-ae-blue">
+          <kbd class="display-4 font-weight-normal bg-transparent">ANTEATER EXPRESS</kbd>
         </div>
-      </React.Fragment>
-
+        <p >Anteater Express is UCI's campus bus system. Our website is the shift scheduling platform for student drivers and administrators. It lets the drivers read, add, remove, trade, and swap desired shifts. Administrators are able to set up initial shifts and accept ordeny changes requested by drivers.</p>
+      </div>
     );
   }
 }
