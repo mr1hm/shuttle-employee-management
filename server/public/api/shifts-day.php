d8d79f49c3f9f1a8dda7173e4ce68d5596527de1
jbg1020 <jbg1020@gmail.com>
2019-08-24 15:12:11 -0700

    got data to display, but it shows on every day

--- server/public/api/shifts-day.php
+++ server/public/api/shifts-day.php
@@ -3,13 +3,13 @@ require_once('functions.php');
 set_exception_handler('error_handler');
 require_once 'db_connection.php';
 
-if (!empty($_GET['startTime'])) {
-  $startTime = $_GET['startTime'];
-  if ($startTime < 600) {
-    throw new Exception('start time is too early');
-  }
-  $startTime = intval($startTime);
-}
+// if (!empty($_GET['startTime'])) {
+//   $startTime = $_GET['startTime'];
+//   if ($startTime < 600) {
+//     throw new Exception('start time is too early');
+//   }
+//   $startTime = intval($startTime);
+// }
 
 // $round_date= $_GET['round_date'];
 
@@ -28,16 +28,18 @@ $query = "SELECT
             rd.`user_id`,
             rd.`start_time`,
             rd.`end_time`,
-            rd.`date`,
+            rd.`round_date`,
             rd.`status`,
-            rt.line_name,
-            rt.id
+            rt.`line_name`,
+            rt.`id`
           FROM
             `round` AS rd
           INNER JOIN
             `route` AS rt
           ON
-            rd.bus_info_id = rt.id";
+            rd.`bus_info_id` = rt.`id`
+          WHERE
+            rd.`status`= 'scheduled' || rd.`status` = 'posted'";
 
 
 $result = mysqli_query($conn, $query);
