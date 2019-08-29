433a4d30afabe9e4733b4dc91104106eb49a8a9d
Juliann <perrigoj@bellsouth.net>
2019-08-29 22:02:50 +0000

    added date to operator query

--- server/public/api/admin-populate-rounds.php
+++ server/public/api/admin-populate-rounds.php
@@ -3,19 +3,20 @@ require_once('functions.php');
 set_exception_handler('error_handler');
 require_once('db_connection.php');
 
-$query = "SELECT rt.line_name, bi.bus_number, 
+$roundsQuery = "SELECT rt.line_name, bi.bus_number, 
 rd.start_time AS round_start, 
 rd.end_time AS round_end,
 us.id AS user_id, 
 us.last_name, 
-us.first_name
+us.first_name,
+rd.date
 FROM route AS rt 
 JOIN bus_info AS bi ON bi.route_id = rt.id 
 JOIN round AS rd ON rd.bus_info_id = bi.id 
 JOIN user AS us ON rd.user_id = us.id
 WHERE rd.session_id = 1";
 
-$result = mysqli_query($conn, $query);
+$result = mysqli_query($conn, $roundsQuery);
 
 if(!$result){
   throw new Exception('MySQL error: '.mysqli_error($conn));
@@ -27,25 +28,24 @@ while ($row = mysqli_fetch_assoc($result)) {
     $rounds[] = $row;
 }
 
-// $rounds = json_encode($rounds);
+$rounds = json_encode($rounds);
 
-// print($rounds);
+print($rounds);
 
 // only includes user info and route preferences
-$query2 ="SELECT us.id,
+$operatorsQuery ="SELECT orp.user_id,
                 us.last_name,
                 us.first_name,
                 GROUP_CONCAT(rt.line_name) AS `lines`,
                 GROUP_CONCAT(JSON_OBJECT('day', oa.day_of_week, 'start_time', oa.start_time, 'stop_time', oa.end_time)) AS `availability`
           FROM user AS us 
-
           JOIN operator_route_preference AS orp ON us.id = orp.user_id
           JOIN operator_availability AS oa ON us.id = oa.user_id
           JOIN route AS rt ON orp.route_id = rt.id
           WHERE orp.session_id = 1
           GROUP BY us.id";
 
-$result2 = mysqli_query($conn, $query2);
+$result2 = mysqli_query($conn, $operatorsQuery);
 
 if(!$result2){
   throw new Exception('MySQL error: '.mysqli_error($conn));
@@ -56,14 +56,13 @@ $operators = [];
 while ($row = mysqli_fetch_assoc($result2)) {
   $row['lines'] = explode(",", $row['lines']);
   $row['lines'] = array_unique($row['lines']);  
-  // $row['lines'] = implode(",", $row['lines']);
+  
   $row['availability'] = explode("},", $row['availability']);
   $row['availability'] = array_unique($row['availability']); 
   $row['availability'] = implode("},", $row['availability']);
 
   $row['availability'] = explode(",{", $row['availability']);
   $row['availability'] = array_unique($row['availability']);
-  $row['availability'] = implode(",{", $row['availability']);
   $operators[] = $row;
 }
 
@@ -84,6 +83,7 @@ print($operatorsForTesting);
 //     else if ($rounds[$roundsIndex]['user_id'] === '1' and $rounds[$roundsIndex + 1]['user_id'] === '1' and $rounds[$roundsIndex + 2]['user_id'] === '1') {    
 //       if ($rounds[$roundsIndex]['line_name'] === $rounds[$roundsIndex + 1]['line_name'] and $rounds[$roundsIndex + 1]['line_name'] === $rounds[$roundsIndex + 2]['line_name']) {
 //         if ($operators[$operatorsIndex]['lines'][0] === $rounds[$roundsIndex]['line_name']){
+//           if($operators[$operatorsIndex]['availability']['day'] === $rounds[$roundsIndex]['date'])
 //           for ($i  = 0; $i < 3; $i++) {
 //             $rounds[$roundsIndex + $i]['user_id'] = $operators[$operatorsIndex]['user_id'];
 //             $rounds[$roundsIndex + $i]['last_name'] = $operators[$operatorsIndex]['last_name'];
