ec20592c3d78cbddb7b86973cfa0081deb0fb716
Stephanie B <sburcombe@gmail.com>
2019-09-03 22:51:04 +0000

    successful connection of post button to fetch call

--- client/components/shifts/details/shifts-details.jsx
+++ client/components/shifts/details/shifts-details.jsx
@@ -29,7 +29,16 @@ class ShiftsDetails extends React.Component {
       .catch(error => {throw(error)});
   }
   postShift(status, userID, roundID){
-    fetch('/api/driver-shift.php' + '?status=' + status + '&user_id=' + userID + '&id=' + roundID, 'PATCH')
+    fetch('/api/driver-shift.php' + '?status=' + status + '&user_id=' + userID + '&id=' + roundID,{
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'PATCH',
+      body: JSON.stringify({ status: status,
+                            user_id: userID,
+                            id: roundID})
+    })
       .then(response => { return response.json()} )
       .then(details => {
         this.setState({
@@ -200,7 +209,7 @@ class ShiftsDetails extends React.Component {
         <Modal open={this.state.isModalOpen} status={this.state.activeModal} shiftStatus={this.state.shiftsDetailsInfo.status}>
           <h2> PLEASE CONFIRM: <br></br>Do you really want to post this shift?</h2>
           <p><button className= "modalCancelButton" onClick= {() => this.closeModal()}>Cancel</button></p>
-          <p><button onClick={() => this.closeModal()} onClick={() => this.postShift('posted', 1, 2)} >Yes, I want to post</button></p> {/* Need to make fetch call to hit driver-shift endpoint to switch shifts from scheduled to posted*/}
+          <p><button  onClick={() => this.postShift('scheduled', 1, 2)} >Yes, I want to post</button></p> {/* Need to make fetch call to hit driver-shift endpoint to switch shifts from scheduled to posted*/}
         </Modal>
       </React.Fragment>
     )
