c3d6771cf3ecfaaf345978d92485a8052badfbda
Stephanie B <sburcombe@gmail.com>
2019-09-27 04:23:05 +0000

    created hard coded base component for routes created hard coded details components in accordion format

--- client/components/admin-routes-buses.jsx
+++ client/components/admin-routes-buses.jsx
@@ -30,64 +30,128 @@ class AdminRoutes extends React.Component {
               <div className="col">Regular Service</div>
           </div>
         </div>
-          <div class="accordion" id="accordionExample">
-            <div class="card">
-              <div class="card-header" id="headingOne">
-                <h2 class="mb-0">
-                  <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Collapsible Group Item #1
-        </button>
-                </h2>
-              </div>
 
-              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                <div class="card-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
+ {/*turn into "accordian component"*/}
+          <div className="accordion" id="accordionExample">
+            <div className="card">
+              <div className="card-header" id="headingOne">
+                <div className="container">
+                  <div className = "row">
+                    <div className = "col">
+                      <RouteBusDisplay route="C"></RouteBusDisplay>
+                    </div>
+                    <div className="col">true</div>
+                    <div className="col">true</div>
+                    <div className="col">true</div>
+                    <button className="btn btn-link col dropdown-toggle" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                      Bus Details
+                   </button>
+                   <button className= "btn btn-dark btn-sm col-sm" type="button"> </button>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="card">
-              <div class="card-header" id="headingTwo">
-                <h2 class="mb-0">
-                  <button class="btn btn-link " type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Collapsible Group Item #2
-        </button>
-                </h2>
-              </div>
-              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                <div class="card-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-              </div>
-            </div>
-            <div class="card">
-              <div class="card-header" id="headingThree">
-                <h2 class="mb-0">
-                  <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Collapsible Group Item #3
-        </button>
-                </h2>
-              </div>
-              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                <div class="card-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-              </div>
-            </div>
-          </div>
-        {/* <div className= "accordian">
-            <div className="card-header" id="headingOne">
-            <div><RouteBusDisplay route="C"></RouteBusDisplay>
-              <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Bus Details</button>
-            </div>
-            </div>
+              {/*turn into bus header component*/}
+              <div id="collapseOne" className="collapse " aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div className="card-body">
+                  <div className="row">
 
-          <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-            <div>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-          </div>
-          </div> */}
+                    <div className="col">#</div>
+                    <div className="col">Start Time</div>
+                    <div className="col">End Time</div>
+                    <div className="col">Rd. Duration</div>
+                    <div className="col">Start Up</div>
+                    <div className="col">Close Down</div>
+                  </div>
+                  <div className="row">
+
+                    <div className="col">
+                      <RouteBusDisplay bus="1"></RouteBusDisplay>
+                    </div>
+                    <div className="col">6:00 a.m.</div>
+                    <div className="col">10:00 a.m.</div>
+                    <div className="col">15 min.</div>
+                    <div className="col">30 min.</div>
+                    <div className="col">45 min.</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/*turn into "accordian route component"*/}
+            <div className="accordion" id="accordionExample">
+              <div className="card">
+                <div className="card-header" id="headingTwo">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col">
+                        <RouteBusDisplay route="D"></RouteBusDisplay>
+                      </div>
+                      <div className="col">true</div>
+                      <div className="col">true</div>
+                      <div className="col">true</div>
+                      <button className="btn btn-link col" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                        Bus Details
+                   </button>
+                    </div>
+                  </div>
+                </div>
+        {/*turn into bus header component*/}
+                <div id="collapseTwo" className="collapse " aria-labelledby="headingTwo" data-parent="#accordionExample">
+                  <div className="card-body">
+                    <div className="row">
+
+                      <div className="col">#</div>
+                      <div className="col">Start Time</div>
+                      <div className="col">End Time</div>
+                      <div className="col">Rd. Duration</div>
+                      <div className="col">Start Up</div>
+                      <div className="col">Close Down</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/*turn into "accordian component"*/}
+              <div className="accordion" id="accordionExample">
+                <div className="card">
+                  <div className="card-header" id="headingThree">
+                    <div className="container">
+                      <div className="row">
+                        <div className="col">
+                          <RouteBusDisplay route="M"></RouteBusDisplay>
+                        </div>
+                        <div className="col">true</div>
+                        <div className="col">true</div>
+                        <div className="col">true</div>
+                        <button className="btn btn-link col" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                          Bus Details
+                   </button>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div id="collapseThree" className="collapse " aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div className="card-body">
+                      <div className="row">
+
+                        <div className="col">#</div>
+                        <div className="col">Start Time</div>
+                        <div className="col">End Time</div>
+                        <div className="col">Rd. Duration</div>
+                        <div className="col">Start Up</div>
+                        <div className="col">Close Down</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                </div>
+                </div>
+                </div>
+
+
+
+
+
+
         </React.Fragment>
       );
     }
