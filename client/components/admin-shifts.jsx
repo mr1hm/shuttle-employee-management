bc111afd61151885eb170ce324c350a767ab4c37
Juliann <perrigoj@bellsouth.net>
2019-10-15 04:16:40 +0000

    auto-populate working on button push from front-end, commented out code removed

--- client/components/admin-shifts.jsx
+++ client/components/admin-shifts.jsx
@@ -10,7 +10,6 @@ class AdminShiftsDay extends React.Component {
     this.query = ``;
     this.fetchCallMethod = this.fetchCallMethod.bind(this);
     this.fetchAutoPopulatedData = this.fetchAutoPopulatedData.bind(this);
-    // this.getAvailableDrivers = this.getAvailableDrivers.bind(this);
     this.dataDidUpdate = this.dataDidUpdate.bind(this);
     const defaultDate = 1566273600; 
     this.state = {
@@ -20,10 +19,7 @@ class AdminShiftsDay extends React.Component {
       dateToPass: defaultDate
     }
   }
-  //I don't think this approach will work.
-  //The autopopulate takes awhile and it is pulling the round data too soon.
-  //It's not noticing that anything has been updated, so the rounds array does not have the 
-  //right information.
+  //updating the db with the autopopulated rounds
   fetchAutoPopulatedData() {
     fetch(`/api/admin-populate-rounds.php`, {
       method: 'POST'
@@ -33,34 +29,8 @@ class AdminShiftsDay extends React.Component {
     })
     .catch(error => { throw (error) });
   }
-  // getAvailableDrivers(){
-  //   fetch(`/api/admin-available-drivers.php`, {
-  //     method: 'GET'
-  //   })
-  //     .then(response => {
-  //       return response.json()
-  //     })
-  //     .then(myJson => {
-  //       this.setState({
-  //         availableOperators: myJson
-  //       })
-  //     })
-  //     .catch(error => { throw (error) });
-  // }
-  // fetchCallMethod(query) {
-  //   fetch(`/api/admin-day-shifts.php` + query, {
-  //     method: 'GET'
-  //   })
-  //     .then(response => {
-  //       return response.json()
-  //     })
-  //     .then(myJson => {
-  //       this.setState({
-  //         rounds: myJson
-  //       })
-  //     })
-  //     .catch(error => { throw (error) });
-  // }
+ 
+  //getting rounds after autopopulation complete
   fetchCallMethod() {
     fetch(`/api/admin-day-shifts.php`, {
       method: 'GET'
@@ -77,7 +47,6 @@ class AdminShiftsDay extends React.Component {
   }
 
   componentDidMount() {
-    // this.fetchAutoPopulatedData();
     this.fetchCallMethod();
   }
   dataDidUpdate(){
@@ -93,10 +62,6 @@ class AdminShiftsDay extends React.Component {
         roundsForLine.push(this.state.rounds[roundsIndex]);
       }
     }
-    // roundsForLine.sort((a, b) => {
-    //   return a.round_start - b.round_start;
-    // });
-    console.log('rounds for line: ', roundsForLine);
     return roundsForLine;
   }
   //build an array of shifts for specific line and bus number
@@ -104,29 +69,19 @@ class AdminShiftsDay extends React.Component {
     var shiftsForLine = [];
     var endTime = null;
     var sortedLineAndBusArray = this.buildRoundsByLine(lineName, busNumber);
-    console.log('sortedLineAndBusArray: ', sortedLineAndBusArray);
     var startTime = sortedLineAndBusArray[0].round_start;
     var userId = sortedLineAndBusArray[0].user_id;
     for (var indexSortedArray = 1;  indexSortedArray < sortedLineAndBusArray.length; indexSortedArray++) {
       if(indexSortedArray === sortedLineAndBusArray.length - 1) {
         // console.log('inside last item')
         endTime = sortedLineAndBusArray[sortedLineAndBusArray.length - 1].round_end;
-        console.log('end of array startTime: ', startTime);
-        console.log('end of array endTime: ', endTime);
-        console.log('end of array Index: ', indexSortedArray);
         shiftsForLine.push({'start_time': startTime, 'end_time': endTime, 'user_id': userId});
         break;
       } 
-      if (sortedLineAndBusArray[indexSortedArray].user_id === userId) {
-        console.log('inside continue')
-        console.log('continue Index: ', indexSortedArray);        
+      if (sortedLineAndBusArray[indexSortedArray].user_id === userId) {     
         continue;
       } else {
-        console.log('inside ELSE item')
         endTime = sortedLineAndBusArray[indexSortedArray - 1].round_end;
-        console.log('mid section startTime: ', startTime);
-        console.log('mid section endTime: ', endTime);
-        console.log('mid section Index: ', indexSortedArray);
         shiftsForLine.push({'start_time': startTime, 'end_time': endTime, 'user_id': userId});
         startTime = sortedLineAndBusArray[indexSortedArray].round_start;
         userId = sortedLineAndBusArray[indexSortedArray].user_id;
@@ -135,10 +90,8 @@ class AdminShiftsDay extends React.Component {
     return shiftsForLine;
   }
   shiftsGroupedByLineAndBus() {
-    // debugger;
     var busAndLineObject = {};
     var groupedShifts = [];
-    // console.log(this.state.rounds);
     
     if (this.state.rounds) {
       for (var index = 0; index < this.state.rounds.length; index++) {
@@ -146,20 +99,16 @@ class AdminShiftsDay extends React.Component {
           busAndLineObject[joinedLineAndBusNumber] = [this.state.rounds[index].line_name, this.state.rounds[index].bus_number];
       }
       //remove the duplicates that still remain in the busAndLineArray
-      console.log('busAndLineObject: ', busAndLineObject);
       for (var key in busAndLineObject) {
         var lineName = busAndLineObject[key][0];
         var busNumber = busAndLineObject[key][1];  
         groupedShifts.push([lineName, busNumber, this.buildShiftsByLine(lineName, busNumber)]);
       }
-      console.log(groupedShifts);
       return groupedShifts;      
     }
   }
   render() {
-    console.log('rounds Array: ', this.state.rounds);
     var groupedArray = this.shiftsGroupedByLineAndBus();
-    console.log('groupedArray: ', groupedArray);
     var range = { min: 600, max: 2400 };
     if (!this.state.rounds) {
       return <div>Retrieving Data</div>;
@@ -189,7 +138,6 @@ class AdminShiftsDay extends React.Component {
                   <div className="dayLabelContainer">
                     <div className="adminShiftsDayBusLine">
                       < RouteBusDisplay
-                        // key={index} 
                         bus={element[1]}  //bus number
                         route={element[0]} //line letter
                       /> 
@@ -197,7 +145,6 @@ class AdminShiftsDay extends React.Component {
                 </div> 
                 <div className="shiftRowContainer"> 
                     < AdminShiftsDisplayComponent
-                      // key={index}
                       range={range}
                       shiftData={{start: 600, end: 2400}}
                       children={element[2]}
