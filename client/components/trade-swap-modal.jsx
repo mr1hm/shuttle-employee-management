7e6336c617fa416aad8e3dccc189158fec334e73
Tarun Padath <tarunpadath@gmail.com>
2019-10-17 12:09:03 -0700

    Got selected driver as a object when it is selected in the dropdown menu of trade/swap

--- client/components/trade-swap-modal.jsx
+++ client/components/trade-swap-modal.jsx
@@ -7,15 +7,39 @@ class TradeSwap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      availableDrivers:[],
+      selectedDriver:{}
     }
+    this.handleDriverClick=this.handleDriverClick.bind(this);
+  }
+  componentDidMount(){
+    this.getAvailableDrivers();
+  }
 
+  handleDriverClick(event){
+    debugger;
+    const driverId= event.target.id;
+    const selectedDriver=this.state.availableDrivers.find(driver => driver.user_id === driverId);
+    this.setState({
+      selectedDriver:selectedDriver
+    })
+  }
+  getAvailableDrivers() {
+    fetch('/api/trade-swap.php?date=1566100800&start_time=700&end_time=720')
+      .then(response =>  response.json() )
+      .then(details => {
+        this.setState({
+          availableDrivers: details
+        })
+      })
+      .catch(error => { throw (error) });
   }
 
   render() {
-    debugger;
     const rounds = this.props.roundArray;
     const timeSpan = this.props.timeSpan;
     const dateAndRound = this.props.dateAndRound;
+    const confirmationText= (this.state.selectedDriver.length >0) ? `Trade or Swap with ${this.state.selectedDriver.first_name} ${this.state.selectedDriver.last_name} ?` : "Please Select Driver";
       return (
         <div className="container d-flex flex-column justify-content-around h-100">
           <div className="row">
@@ -27,9 +51,14 @@ class TradeSwap extends React.Component {
                 Select Coworker
             </button>
               <div className="dropdown-menu w-100">
-                ...
+                {this.state.availableDrivers.map(singleDriver => {
+                  return <button onClick={this.handleDriverClick} className="dropdown-item" type="button" id={singleDriver.user_id} key={singleDriver.user_id}>{singleDriver.first_name} {singleDriver.last_name}</button>
+                })}
             </div>
             </div>
+          </div>
+          <div className="row justify-content-center">
+            <div>{confirmationText}</div>
           </div>
           <div className="row justify-content-center h-25">
             <div className="col-1">
