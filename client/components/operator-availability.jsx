1d228d993da3962b037e8ddfb87b16b11560a32d
Juliann <perrigoj@bellsouth.net>
2019-10-22 13:42:40 +0000

    clean-up to make code more compact and readable, code in main render method could use some more work it reduce the number of lines -- still repeated code there

--- client/components/operator-availability.jsx
+++ client/components/operator-availability.jsx
@@ -18,83 +18,73 @@ class OperatorAvailability extends React.Component {
     }
   }
 
-  buildBlock(day) {
+  buildDayCell(day) {
     return(
       this.state.availability[day].map((element, index) => {
         if(element) {
-          return <td key={index} style={{backgroundColor: 'green', width: '1.355%', height: '10vh', border: '1px solid black'}}></td>
+          return <td key={index} style={{backgroundColor: 'green', width: '1.35%', height: '10vh', border: '1px solid black'}}></td>
         } else {
-          return <td key={index} style={{backgroundColor: 'lightgrey', width: '1.355%', height: '10vh', border: '1px solid black'}}></td>
+          return <td key={index} style={{backgroundColor: 'lightgrey', width: '1.35%', height: '10vh', border: '1px solid black'}}></td>
         }
       })
     );
   }
 
-  render() {
-    const divStyle = {width: '2.5vw', height: '10vh', borderWidth: '1px'}; 
-    const divDay = {width: '2.5vw', height: '10vh', borderWidth: '1px'}; 
+  buildHeaderTimes() {
+    const headerTimes = ['6am', '7am', '8am', '9am', '10am','11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm','11pm'];
 
+    return(
+    headerTimes.map((element,index) => {
+      return <th key={index} style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>{element}</th>
+    })
+    );
+  }
+
+  render() {
     return (
         <React.Fragment>
           <TopMenuGeneral title="MY AVAILABILITY"/>
           <div className="d-flex">
-          <div style={{width: '5%'}}></div>
-          <table className="mt-5 mr-0" style={{width: '90%'}}>
-            <thead className="container">
-              <tr className="row d-flex justify-content-end"> 
-                <th style={{width: '2.8%', height: '5vh', border: '1px solid black'}}>Day</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>6 am</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>7am</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>8am</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>9am</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>10am</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>11am</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>12pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>1pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>2pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>3pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>4pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>5pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>6pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>7pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>8pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>9pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>10pm</th>
-                <th style={{width: '5.4%', height: '5vh', border: '1px solid black'}}>11pm</th>
-              </tr>
-            </thead>
-            <tbody className="container">
-              <tr className="row d-flex justify-content-end">
-                <td className= "align-middle" style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Sun</td>
-                {this.buildBlock('Sunday')}
-              </tr>
-              <tr className="row d-flex justify-content-end">
-                <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Mon</td>
-                {this.buildBlock('Monday')}
-              </tr>
-              <tr className="row d-flex justify-content-end">
-                <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Tue</td>
-                {this.buildBlock('Tuesday')}
-              </tr>
-              <tr className="row d-flex justify-content-end">
-                <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Wed</td>
-                {this.buildBlock('Wednesday')}
-              </tr>
-              <tr className="row d-flex justify-content-end">
-                <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Thu</td>
-                {this.buildBlock('Thursday')}
-              </tr>
-              <tr className="row d-flex justify-content-end">
-                <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Fri</td>
-                {this.buildBlock('Friday')}
-              </tr>
-              <tr className="row d-flex justify-content-end">
-                <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Sat</td>
-                {this.buildBlock('Saturday')}
-              </tr>
-            </tbody>
-          </table>
-          <div style={{width: '5%'}}></div>
+            <div style={{width: '5%'}}></div>
+            <table className="mt-5 mr-0" style={{width: '90%'}}>
+              <thead className="container">
+                <tr className="row d-flex justify-content-end"> 
+                  <th style={{width: '2.8%', height: '5vh', border: '1px solid black'}}>Day</th>
+                  {this.buildHeaderTimes()}
+                </tr>
+              </thead>
+              <tbody className="container">
+                <tr className="row d-flex justify-content-end">
+                  <td className= "align-middle" style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Sun</td>
+                  {this.buildDayCell('Sunday')}
+                </tr>
+                <tr className="row d-flex justify-content-end">
+                  <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Mon</td>
+                  {this.buildDayCell('Monday')}
+                </tr>
+                <tr className="row d-flex justify-content-end">
+                  <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Tue</td>
+                  {this.buildDayCell('Tuesday')}
+                </tr>
+                <tr className="row d-flex justify-content-end">
+                  <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Wed</td>
+                  {this.buildDayCell('Wednesday')}
+                </tr>
+                <tr className="row d-flex justify-content-end">
+                  <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Thu</td>
+                  {this.buildDayCell('Thursday')}
+                </tr>
+                <tr className="row d-flex justify-content-end">
+                  <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Fri</td>
+                  {this.buildDayCell('Friday')}
+                </tr>
+                <tr className="row d-flex justify-content-end">
+                  <td style={{backgroundColor: 'white', width: '2.8%',  heigth: '10vh', border: '1px solid black'}}>Sat</td>
+                  {this.buildDayCell('Saturday')}
+                </tr>
+              </tbody>         
+            </table>
+            <div style={{width: '5%'}}></div>
           </div>
         </React.Fragment>
     );
