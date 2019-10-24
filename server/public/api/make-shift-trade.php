b487acb09e94a6226df367c102fccac27d7a0a87
danemaison <danemaison@gmail.com>
2019-10-24 00:51:43 +0000

    update make-shift-trade.php

--- server/public/api/make-shift-trade.php
+++ server/public/api/make-shift-trade.php
@@ -6,20 +6,13 @@ $input = getBodyData();
 
 $user_id = intval($input['user_id']);
 $target_id = intval($input['target_id']);
-$user_round = $input['user_round'];
+$user_round = intval($input['user_round']);
 
-mysqli_query($conn, 'START TRANSACTION');
-
-forEach($user_round as $round){
-  $round = intval($round);
-  $query = "UPDATE `round` SET `user_id` = {$target_id} WHERE `id` = {$round}";
-  $result = mysqli_query($conn, $query);
-  if (mysqli_affected_rows($conn) < 1) {
-    mysqli_query($conn, "ROLLBACK");
-    throw new Exception(mysqli_error($conn));
-  }
+$query = "UPDATE `round` SET `user_id` = {$target_id} WHERE `id` = {$user_round}";
+$result = mysqli_query($conn, $query);
+if (mysqli_affected_rows($conn) < 1) {
+  throw new Exception(mysqli_error($conn));
 }
 
-mysqli_query($conn, "COMMIT");
 
 ?>
