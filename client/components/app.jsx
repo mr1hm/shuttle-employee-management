14e79bac545ec68bf4b63a8ea31c35a83e23e85d
Juliann <perrigoj@bellsouth.net>
2019-10-30 03:40:10 +0000

    removed add/delete buttons, added click functionality, added ability to delete shift

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -11,6 +11,8 @@ import Welcome from './welcome/welcome';
 import Modal from './post-modal';
 import OperatorAvailability from './operator-availability';
 import OperatorAvailability2 from './operator-availability2';
+import OperatorAvailability3 from './operator-availability3';
+import OperatorAvailability4 from './operator-availability4';
 import AdminShiftsDay from './admin-shifts';
 import AdminRoutes from './admin-lines-buses';
 import Transaction from './transaction/transactionpage';
@@ -65,6 +67,10 @@ class App extends React.Component {
             <Route path="/admin-day/" render={props => <AdminShiftsDay userId={this.state.userId ? this.state.userId : 17} {...props} defaultDate={this.state.presetDateForTesting} />} />
             <Route path="/operator-availability/" render={props => <OperatorAvailability userId={this.state.userId ? this.state.userId : 17} />} />
             <Route path="/operator-availability2/" render={props => <OperatorAvailability2 userId={this.state.userId ? this.state.userId : 17} />} />
+            <Route path="/operator-availability3/" render={props => <OperatorAvailability3 userId={this.state.userId ? this.state.userId : 17} />} />
+
+            <Route path="/operator-availability4/" render={props => <OperatorAvailability4 userId={this.state.userId ? this.state.userId : 17} />} />
+
             <Route path="/trade-swap/" render={props => <TradeSwap {...props} shiftDetails={this.state.shiftDetails} />} />
           </Switch>
         </React.Fragment>
@@ -85,7 +91,13 @@ class App extends React.Component {
             <Route path = "/admin-day/" render={props => <AdminShiftsDay {...props} defaultDate={this.state.presetDateForTesting} />}/>
             <Route path="/admin-routes/" render={props => <AdminRoutes {...props} defaultDate={this.state.presetDateForTesting} />} />
             <Route path="/operator-availability/" render={props => <OperatorAvailability {...props}/>}/>
+
             <Route path="/operator-availability2/" render={props => <OperatorAvailability2 {...props}/>}/>
+
+            <Route path="/operator-availability3/" render={props => <OperatorAvailability3 {...props}/>}/>
+
+            <Route path="/operator-availability4/" render={props => <OperatorAvailability4 {...props}/>}/>
+
             <Route path="/trade-swap/" render={props => <TradeSwap {...props}/>} />
           </Switch>
         </React.Fragment>
---
+++ client/components/operator-availability3.jsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import TableDragSelect from 'react-table-drag-select';
+import 'react-table-drag-select/style.css';
+// import TableDragSelect from './table-drag-select';
+// import 'react-table-drag-select/style.css';
+
+import TopMenuGeneral from './topmenu/topmenu-general';
+import './operator-availability.css';
+// import SubmitModal from './operator-submit-modal';
+
+class OperatorAvailability3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cells: [
+        [false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false]
+      ]
+    };
+    this.handleReset = this.handleReset.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <TopMenuGeneral title="MY AVAILABILITY"/>
+        <TableDragSelect value={this.state.cells}
+          onChange={this.handleChange}>
+          <tr>
+            <td disabled />
+            <td disabled>Sunday</td>
+            <td disabled>Monday</td>
+            <td disabled>Tuesday</td>
+            <td disabled>Wednesday</td>
+            <td disabled>Thursday</td>
+            <td disabled>Friday</td>
+            <td disabled>Saturday</td>
+          </tr>
+          <tr>
+            <td disabled>10:00</td>
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+          <tr>
+            <td disabled>11:00</td>
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+          <tr>
+            <td disabled>12:00</td>
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+          <tr>
+            <td disabled>13:00</td>
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+          <tr>
+            <td disabled>14:00</td>
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+          <tr>
+            <td disabled>15:00</td>
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+          <tr>
+            <td disabled>16:00</td>
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+        </TableDragSelect>
+        <button onClick={this.handleReset}>Reset</button>
+      </React.Fragment>
+    );
+  }
+
+  handleChange(cells) {
+    this.setState({
+      cells
+    });
+  }
+
+  handleReset() {
+    const cells = [
+      [false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false]
+    ];
+    this.setState({
+      cells: cells
+    });
+  }
+}
+
+export default OperatorAvailability3;
