c213f9833d29fbff473fd34d0ceaf244dc9be972
Mitchell O'Hair <mitchell.ohair@gmail.com>
2019-11-13 20:39:18 -0800

    Removed commented out function that was moved to another file

--- server/public/api/admin-populate-rounds.php
+++ server/public/api/admin-populate-rounds.php
@@ -64,18 +64,6 @@ function getShift ($lineName, &$rounds) {
   return $shift;
 }
 
-  // function updateShiftFlags (&$operator, $shift) {
-  //   if (intval($operator['minutes_without_30_minute_break']) >= 300) {
-  //     $operator['minutes_without_30_minute_break'] -= 300;
-  //   }
-  //   if ( intval(end($shift)['round_end']) > 2200 ) {
-  //     $operator['shift_restrictions']['worked_passed_10']['current_day'] = true;
-  //   }
-  //   if ( intval($operator['assigned_times'][0][0]) < 800 ) {
-  //     $operator['shift_restrictions']['shift_passed_15_hour_window']['shift_start'] = intval($operator['assigned_times'][0][0]);
-  //   }
-  // }
-
   /* Make sure:
    * - All of the rounds in this shift are unassigned to start with
    * - All of the rounds in this shift are on the same line
@@ -96,107 +84,6 @@ function getShift ($lineName, &$rounds) {
 
     return true;
   }
-
-  // /* Update operators times_assigned associative array with shift times
-  //  * Update operators times_available associative array
-  //  * Update operators daily & weekly minutes */
-  // function assignShiftToOperator (&$operator, $shift) {
-  //   updateOperatorAssignedTimes($operator, $shift);
-  //   updateOperatorAvailableTimes($operator, $shift);
-
-  //   $shiftTime = calculateShiftMinutes(intval(reset($shift)['round_start']), intval(end($shift)['round_end']));
-  //   $operator['minutes_without_30_minute_break'] += $shiftTime;
-  //   $operator['total_daily_minutes'] += $shiftTime;
-  //   $operator['total_weekly_minutes'] += $shiftTime;
-  // }
-
-  // function updateOperatorAssignedTimes (&$operator, $shift) {
-  //   $shiftStart = intval(reset($shift)['round_start']);
-  //   $shiftEnd = intval(end($shift)['round_end']);
-  //   $lastIndex = count($operator['assigned_times']) - 1;
-
-  //   // Empty assigned times
-  //   if ( empty($operator['assigned_times']) ) {
-  //     array_push($operator['assigned_times'], [$shiftStart, $shiftEnd]);
-  //     return;
-  //   }
-
-  //   // Insert at beginning
-  //   if ( $shiftEnd <= intval($operator['assigned_times'][0][0]) ) {
-  //     array_unshift($operator['assigned_times'], [$shiftStart, $shiftEnd]);
-  //     if ( intval($operator['assigned_times'][0][1]) === intval($operator['assigned_times'][1][0]) ) {
-  //       $operator['assigned_times'][0][1] = $operator['assigned_times'][1][1];
-  //       array_splice($operator['assigned_times'], 1, 1);
-  //     }
-  //     return;
-  //   }
-  //   // Insert at end
-  //   if ($shiftStart >= intval($operator['assigned_times'][$lastIndex][1])) {
-  //     array_push($operator['assigned_times'], [$shiftStart, $shiftEnd]);
-  //     $lastIndex = count($operator['assigned_times']) - 1;
-  //     if (intval($operator['assigned_times'][$lastIndex][0]) === intval($operator['assigned_times'][$lastIndex - 1][1])) {
-  //       $operator['assigned_times'][$lastIndex - 1][1] = $operator['assigned_times'][$lastIndex][1];
-  //       array_pop($operator['assigned_times']);
-  //     }
-  //     return;
-  //   }
-
-  //   // Insert in between
-  //   $prevTimeBlock = null;
-  //   $insertIndex = 0;
-  //   foreach ( $operator['assigned_times'] as $key => &$timeBlock ) {
-  //     if ( $prevTimeBlock && ($shiftStart >= $prevTimeBlock[1] && $shiftEnd <= $timeBlock[0]) ) {
-  //       array_splice($operator['assigned_times'], $key, 1, [ [$shiftStart, $shiftEnd] ]);
-  //       $insertIndex = $key;
-  //       break;
-  //     }
-  //     $prevTimeBlock = $timeBlock;
-  //   }
-  //   unset($timeBlock);
-
-  //   /* If inserted shift has start/end times that are equivalent to neighboring
-  //     * shifts - trunctate the shift(s) into a single shift */
-  //   if (intval($operator['assigned_times'][$insertIndex - 1][1]) === intval($operator['assigned_times'][$insertIndex][0])) {
-  //     $operator['assigned_times'][$insertIndex - 1][1] = $operator['assigned_times'][$insertIndex][1];
-  //     array_splice($operator['assigned_times'], $insertIndex, 1);
-  //     --$insertIndex;
-  //   }
-  //   if ( isset($operator['assigned_times'][$insertIndex + 1]) ) {
-  //     if (intval($operator['assigned_times'][$insertIndex + 1][0]) === intval($operator['assigned_times'][$insertIndex][1])) {
-  //       $operator['assigned_times'][$insertIndex + 1][0] = $operator['assigned_times'][$insertIndex][0];
-  //       array_splice($operator['assigned_times'], $insertIndex, 1);
-  //     }
-  //   }
-  // }
-
-  // function updateOperatorAvailableTimes (&$operator, $shift) {
-  //   // Remove availability if it is the same as the shift
-  //   $key = array_search($shift, array_column($operator, 'available_times'));
-  //   if ( $key !== false ) {
-  //     array_splice($operator['available_times'], $key, 1);
-  //     return;
-  //   }
-
-  //   $shiftStart = intval(reset($shift)['round_start']);
-  //   $shiftEnd = intval(end($shift)['round_end']);
-  //   foreach ( $operator['available_times'] as $key => &$timeBlock ) {
-  //     if ( $shiftStart === intval($timeBlock[0]) ) { // Push back start time
-  //       $timeBlock[0] = $shiftEnd;
-  //       break;
-  //     }
-  //     if ( $shiftEnd === intval($timeBlock[1]) ) { // Push back end time
-  //       $timeBlock[1] = $shiftStart;
-  //       break;
-  //     }
-  //     // If shift is inside of a time block - Break the time block into 2 blocks
-  //     if ( $shiftStart > intval($timeBlock[0]) && $shiftEnd < intval($timeBlock[1]) ) {
-  //       $newBlockEnd = intval($timeBlock[1]);
-  //       $timeBlock[1] = $shiftStart;
-  //       array_splice($operator['available_times'], $key+1, 1, [$key + 1 => [$shiftEnd, $newBlockEnd]]);
-  //       break;
-  //     }
-  //   }
-  // }
 
   // Update each round in the shift with the operators information
   function assignOperatorToShift ($operator, &$shift) {
