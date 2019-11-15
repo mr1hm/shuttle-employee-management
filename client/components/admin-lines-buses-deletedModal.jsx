72d4f35225004eaebbd330572ddba380437df8c5
Kevin Ihm <kevinihm@gmail.com>
2019-11-15 12:10:14 -0800

    added deleted confirmation modal

---
+++ client/components/admin-lines-buses-deletedModal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class DeleteConfirmationModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deleted: null
+    };
+  }
+
+  render() {
+    return null;
+  }
+}
