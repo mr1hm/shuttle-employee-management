e96bff2a5f7c4a2b78501b54a8e4c5f42dbe1412
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-03 04:26:10 +0000

    Refactored login to verify password and send back user and role info

--- server/public/api/login-page.php
+++ server/public/api/login-page.php
@@ -1,19 +1,66 @@
 <?php
-require_once('functions.php');
-set_exception_handler('error_handler');
-require_once('db_connection.php');
-$email = $_POST['email'];
-$password = $_POST['password'];
-$query = "SELECT user.id FROM `user` WHERE user.email='$email'AND user.uci_net_id='$password'";
-$result = mysqli_query($conn, $query);
-if (!$result) {
-  throw new Exception('mysql error ' . mysqli_error($conn));
-}
-$data = [];
-while ($row = mysqli_fetch_assoc($result)) {
-  $data[] = $row;
-}
+  require_once('start_up.php');
 
-print(json_encode(array_values($data[0])));
+  $error_msg = 'Email password combination do not match';
+  $errors = [];
 
+  if(isset($_POST['email'])) {
+    $email = $_POST['email'];
+  } else {
+    $errors[] = 'Missing email';
+  }
+
+  if(isset($_POST['password'])) {
+    $password = $_POST['password'];
+  } else {
+    $errors[] = 'Missing password';
+  }
+
+  if(count($errors)) {
+    throw new ApiError(['errors' => $errors], 422);
+  }
+
+  $stmt = $mysqli->prepare('
+    SELECT uci_net_id AS uciNetId, `password`, email, CONCAT(first_name, " ", last_name) AS `name`, CONCAT("[", GROUP_CONCAT(JSON_OBJECT("mid", r.mid, "name", r.display_name)), "]") AS roles
+    FROM `user` AS u
+    LEFT JOIN user_roles as ur
+    ON u.id=ur.user_id
+    LEFT JOIN roles AS r
+    ON ur.role_id=r.id
+    WHERE email=?
+    GROUP BY uciNetId, `password`, name, email
+  ');
+
+  if(!$stmt) {
+    throw new ApiError(null, 500, $mysqli->error);
+  }
+
+  $stmt->bind_param('s', $email);
+  $stmt->execute();
+
+  $result = $stmt->get_result();
+
+  if(!$result->num_rows) {
+    throw new ApiError(null, 401, $error_msg);
+  }
+
+  $user = $result->fetch_assoc();
+
+  $password_match = password_verify($password, $user['password']);
+
+  if(!$password_match) {
+    throw new ApiError(null, 401, $error_msg);
+  }
+
+  $user['roles'] = $roles = json_decode($user['roles'], 1);
+
+  if(count($roles) === 1 && $roles[0]['mid'] === null) {
+    $user['roles'] = [];
+  }
+
+  unset($user['password']);
+
+  $SESSION['user'] = $user;
+
+  send($user, 201);
 ?>
