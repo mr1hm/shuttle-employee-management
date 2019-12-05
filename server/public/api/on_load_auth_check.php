d2fb458849b7eb57abc18296bbf97f287bd5cdb9
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-05 21:13:31 +0000

    Updated to use new auth helper functions

--- server/public/api/on_load_auth_check.php
+++ server/public/api/on_load_auth_check.php
@@ -1,23 +1,16 @@
 <?php
 require('../../lib/start_up.php');
 require(AUTH);
-$user_data = null;
 
-if(isset($_SESSION['user'])) {
-    $user_data = $_SESSION['user'];
-} else if(isset($_SERVER[AUTH_HEADER])) {
-    $token = $_SERVER[AUTH_HEADER];
+$user = getRequestUser();
 
-    $user_data = decrypt($token);
-}
-
-if(!$user_data) {
+if(!$user) {
     send(null, 204);
     exit;
 }
 
-$user = getUserByUciNetId($user_data['uciNetId']);
-
 $_SESSION['user'] = buildUserSessionData($user);
+
+cleanUser($user);
 
 send($user);
