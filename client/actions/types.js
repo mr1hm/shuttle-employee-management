560594a0b1ee97942750a9ba81699b0663268984
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-06 10:19:02 -0800

    Added new types for getting a users profile data

--- client/actions/types.js
+++ client/actions/types.js
@@ -1,4 +1,6 @@
 export default {
+  GET_USER_PROFILE: 'get_user_profile',
+  GET_USER_PROFILE_ERROR: 'get_user_profile_error',
   TEMP_LOGIN: 'temp_login',
   USER_LOGIN: 'user_login',
   USER_LOGOUT: 'user_logout'
