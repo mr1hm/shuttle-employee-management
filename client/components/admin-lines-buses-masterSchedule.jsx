d11cee780d53bff01e217f665193458caf059727
mr1hm <kevinihm@gmail.com>
2019-12-06 11:20:40 -0800

    live field status and master schedule now show proper shifts

--- client/components/admin-lines-buses-masterSchedule.jsx
+++ client/components/admin-lines-buses-masterSchedule.jsx
@@ -18,11 +18,14 @@ export default class MasterSchedule extends React.Component {
       currentShift: null,
       upcomingShift: null,
       driverName: null,
-      allShifts: []
+      allShifts: [],
+      dateSelection: [],
+      selectedDate: null,
+      shiftsOnSelectedDate: null
     };
-    this.allShifts = [];
-    this.checkAndDisplayShifts = this.checkAndDisplayShifts.bind(this);
     this.compareCurrentTimeAndDisplayShifts = this.compareCurrentTimeAndDisplayShifts.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.getDataOnSelectedDate = this.getDataOnSelectedDate.bind(this);
   }
 
   componentDidMount() {
@@ -80,6 +83,40 @@ export default class MasterSchedule extends React.Component {
     // }
   }
 
+  handleDateChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    let reformatValue = value.split('/');
+
+    let month = reformatValue[0];
+    let year = reformatValue[2];
+    let day = reformatValue[1];
+    reformatValue[0] = reformatValue[2];
+    reformatValue[1] = month;
+    reformatValue[2] = day;
+    reformatValue.splice(1, 0, '-');
+    reformatValue.splice(3, 0, '-');
+    console.log(reformatValue);
+    let formattedDate = reformatValue.join('');
+    console.log(formattedDate);
+    this.setState({ [name]: formattedDate }, this.getDataOnSelectedDate);
+  }
+
+  getDataOnSelectedDate() {
+    const { selectedDate } = this.state;
+    const body = { masterSchedule: selectedDate };
+    const init = { method: 'POST', body: JSON.stringify(body) };
+
+    fetch(`api/admin-lines-buses.php`, init)
+      .then(response => response.json())
+      .then(shiftsOnSelectedDate => {
+        this.setState({
+          roundInfoToday: shiftsOnSelectedDate
+        }, this.compareCurrentTimeAndDisplayShifts);
+      })
+      .catch(error => console.error(error));
+  }
+
   compareCurrentTimeAndDisplayShifts() {
     this.getCurrentSession();
     const { roundInfoToday } = this.state;
@@ -94,7 +131,6 @@ export default class MasterSchedule extends React.Component {
     // For each object in roundInfoToday store the line name, and Vehicle ID
     // loop through the rounds in that object
     // condense each shift by start time, end time, userID
-
     const shifts = [];
     for (let i = 0; i < roundInfoToday.length; i++) {
       const { line_name, bus_number, vehicle_id, shifts: rounds } = roundInfoToday[i];
@@ -142,6 +178,30 @@ export default class MasterSchedule extends React.Component {
     let displayCurrentDate = mm + '/' + dd + '/' + yyyy;
     let dateToCompare = yyyy + '-' + mm + '-' + dd;
 
+    let day = dd;
+    for (let i = 0; i < 7; i++) {
+      const { dateSelection } = this.state;
+      if (i === 0) {
+        dateSelection.push(`${mm}/${dd}/${yyyy}`);
+      }
+      if (day[0] === '0') {
+        parseInt(day);
+        day++;
+        day += '';
+        if (day.length < 2) {
+          day = '0' + day;
+        }
+        console.log('day', day);
+        dateSelection.push(`${mm}/${day}/${yyyy}`);
+      } else {
+        parseInt(day);
+        day++;
+        day += '';
+        dateSelection.push(`${mm}/${day}/${yyyy}`);
+      }
+      console.log(dateSelection);
+    }
+
     switch (currentDayOfTheWeek) {
       case 0:
         currentDayOfTheWeek = 'Sunday';
@@ -169,42 +229,6 @@ export default class MasterSchedule extends React.Component {
       currentDayOfTheWeek,
       displayCurrentDate,
       dateToCompare
-    });
-  }
-
-  checkAndDisplayShifts() {
-    const { driversForToday } = this.state;
-    const { dateToCompare } = this.state;
-    const currentTime = new Date().toLocaleTimeString();
-    let currentHour = new Date().getHours() + '';
-    let currentMin = new Date().getMinutes() + '';
-    if (currentHour.length === 1) {
-      currentHour = '0' + currentHour;
-    } else if (currentMin.length === 1) {
-      currentMin = '0' + currentMin;
-    }
-    const current24Time = currentHour + '' + currentMin;
-    console.log(current24Time);
-    driversForToday.forEach(driver => {
-      driver.shifts.forEach(shift => {
-        const current24TimeInt = parseInt(current24Time);
-        if (parseInt(shift[1]) < current24TimeInt) {
-          this.setState({
-            prevShift: `${shift[0]} - ${shift[1]}`
-          });
-        } else if (parseInt(shift[0]) > current24TimeInt) {
-          this.setState({
-            upcomingShift: `${shift[0]} - ${shift[1]}`
-          });
-        } else if (current24TimeInt > parseInt(shift[0]) && current24TimeInt < parseInt(shift[1])) {
-          this.setState({
-            currentShift: `${shift[0]} - ${shift[1]}`
-          });
-        }
-        this.setState({
-          driverName: driver.name
-        });
-      });
     });
   }
 
@@ -240,13 +264,13 @@ export default class MasterSchedule extends React.Component {
   }
 
   render() {
-    const { currentSession, allShifts } = this.state;
+    const { roundInfoToday, currentSession, allShifts, dateSelection } = this.state;
     if (!currentSession) {
       return <div>LOADING...</div>;
     }
-    if (allShifts.length === 0) {
-      return <div>LOADING...</div>;
-    }
+    // if (allShifts.length === 0) {
+    //   return <div>LOADING...</div>;
+    // }
     return (
       <>
         <div className="container-fluid">
@@ -255,9 +279,6 @@ export default class MasterSchedule extends React.Component {
               <div className="col">
                 <img className="anteaterMascot" src={require('../../server/public/assets/images/mascot/anteater.png')} alt="anteater mascot" />
                 <h3 className="liveFieldStatusHeaderTitle">Anteater<br /> Express</h3>
-              </div>
-              <div className="col d-flex justify-content-end align-items-end">
-                <h5>ADMIN</h5>
               </div>
             </div>
           </header>
@@ -271,8 +292,18 @@ export default class MasterSchedule extends React.Component {
               <div className="col-12 d-flex justify-content-center">
                 <span className="masterScheduleCurrentDay">{this.state.currentDayOfTheWeek}</span>
               </div>
+              <div className="col-12 d-flex justify-content-center">
+                <span className="masterScheduleCurrentDateAndTime">{this.state.currentTime}</span>
+              </div>
+              <br/>
               <div className="col d-flex justify-content-center">
-                <span className="masterScheduleCurrentDateAndTime">{this.state.displayCurrentDate} : {this.state.currentTime} </span>
+                <select onChange={this.handleDateChange} className="col-1 d-flex justify-content-center border-primary" name="selectedDate">
+                  {dateSelection.map((date, index) => {
+                    return (
+                      <option key={date + index}>{date}</option>
+                    );
+                  })}
+                </select>
               </div>
             </div>
           </div>
@@ -282,7 +313,9 @@ export default class MasterSchedule extends React.Component {
               <div className="col d-flex ml-5">
                 <h2 className="liveFieldStatusSessionName">{currentSession.name}</h2>
               </div>
-              <div className="col d-inline-flex align-items-end justify-content-end">
+              {allShifts.length === 0
+                ? <div className="col d-flex align-items-end masterScheduleNoDrivers">THERE ARE NO DRIVERS FOR THIS DAY</div> : null}
+              <div className="col d-inline-flex align-items-end justify-content-end masterScheduleStartEndDate">
                 <div>
                   <span className="liveFieldStatusStartDateSpan"><i className="liveFieldStatusStartDate">Start Date</i>: {currentSession.startDateString}</span>
                   <span><i className="liveFieldStatusEndDate">End Date</i>: {currentSession.endDateString}</span>
@@ -292,7 +325,7 @@ export default class MasterSchedule extends React.Component {
 
               </div>
             </div>
-            <div className="container liveFieldStatusTableContainer">
+            <div className="container masterScheduleTableContainer">
               <div className="row">
                 <table className="table liveFieldStatusTable">
                   <thead>
@@ -305,17 +338,18 @@ export default class MasterSchedule extends React.Component {
                     </tr>
                   </thead>
                   <tbody>
-                    {allShifts.map((driver, index) => {
-                      return (
-                        <tr key={driver.role + index}>
-                          <td className="masterScheduleLineName">{driver.line_name}</td>
-                          <td>{driver.bus_number}</td>
-                          <td>{`AE-${driver.vehicle_id}`}</td>
-                          <td>{driver.name}</td>
-                          <td>{`${driver.start} - ${driver.end}`}</td>
-                        </tr>
-                      );
-                    })}
+                    {allShifts.length !== 0
+                      ? allShifts.map((driver, index) => {
+                        return (
+                          <tr key={driver.role + index}>
+                            <td className="masterScheduleLineName">{driver.line_name}</td>
+                            <td>{driver.bus_number}</td>
+                            <td>{`AE-${driver.vehicle_id}`}</td>
+                            <td>{`${driver.name} (${driver.role})`}</td>
+                            <td>{`${driver.start} - ${driver.end}`}</td>
+                          </tr>
+                        );
+                      }) : null}
                   </tbody>
                 </table>
               </div>
