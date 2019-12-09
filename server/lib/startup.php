bb9d5a860a48d0efeae81416ce7f1890f3093bf9
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-09 10:50:27 -0800

    Created constant for DATES lib file path

--- server/lib/startup.php
+++ server/lib/startup.php
@@ -4,8 +4,9 @@ require_once(__DIR__.'/error_handler.php');
 require_once(__DIR__.'/../config/db_connect.php');
 
 define('LIB', __DIR__);
-define('ENCRYPT', LIB.'/encrypt.php');
 define('AUTH', LIB.'/auth.php');
+define('DATES', LIB.'/dates.php');
+define('ENCRYPT', LIB.'/encrypt.php');
 
 function send($data, $status = 200) {
   http_response_code($status);
