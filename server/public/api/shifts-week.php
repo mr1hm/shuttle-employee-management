31c08232405d8ac30ceabfec805544c00d4115ab
Tim Horist <TimelikeClosure@users.noreply.github.com>
2019-12-09 19:56:10 -0800

    update shifts-week.php fix indenting

--- server/public/api/shifts-week.php
+++ server/public/api/shifts-week.php
@@ -5,79 +5,79 @@ require_once(AUTH);
 require_once(DATES);
 set_tz_la();
 
-  $user = getRequestUser();
+$user = getRequestUser();
 
-  if(!$user) {
-    throw new ApiError(null, 401, 'Not Authorized');
-  }
+if(!$user) {
+  throw new ApiError(null, 401, 'Not Authorized');
+}
 
-  $startDate = getLast('Sunday');
-  $endDate = getNext('Saturday');
+$startDate = getLast('Sunday');
+$endDate = getNext('Saturday');
 
-  if(isset($_GET['startDate']) && $_GET['endDate']) {
-    $startDate = $_GET['startDate'];
-    $endDate = $_GET['endDate'];
-  }
+if(isset($_GET['startDate']) && $_GET['endDate']) {
+  $startDate = $_GET['startDate'];
+  $endDate = $_GET['endDate'];
+}
 
-  $stmt = $mysqli->prepare("SELECT
-            rd.`bus_info_id`,
-            rd.`user_id`,
-            rd.`start_time`,
-            rd.`end_time`,
-            rd.`date`,
-            rt.`line_name`,
-            rt.`id`,
-            bi.`route_id`,
-            bi.`bus_number`,
-            bi.`id`,
-            rd. `status`,
-            rd.`id` AS roundID
+$stmt = $mysqli->prepare("SELECT
+          rd.`bus_info_id`,
+          rd.`user_id`,
+          rd.`start_time`,
+          rd.`end_time`,
+          rd.`date`,
+          rt.`line_name`,
+          rt.`id`,
+          bi.`route_id`,
+          bi.`bus_number`,
+          bi.`id`,
+          rd. `status`,
+          rd.`id` AS roundID
 
 
-          FROM
-            `round` AS rd
-          INNER JOIN
-          	`bus_info` as bi
-            ON
-            rd.`bus_info_id` = bi.`id`
-          INNER JOIN
-            `route` AS rt
+        FROM
+          `round` AS rd
+        INNER JOIN
+          `bus_info` as bi
           ON
-            rt.id = bi.`route_id`
-          WHERE `user_id`= ? AND (`date` >= ? AND `date` <= ?)
-          ORDER BY `date`, `start_time` ASC");
+          rd.`bus_info_id` = bi.`id`
+        INNER JOIN
+          `route` AS rt
+        ON
+          rt.id = bi.`route_id`
+        WHERE `user_id`= ? AND (`date` >= ? AND `date` <= ?)
+        ORDER BY `date`, `start_time` ASC");
 
-  $stmt->bind_param('iss', $user['userId'], $startDate, $endDate);
-  $stmt->execute();
+$stmt->bind_param('iss', $user['userId'], $startDate, $endDate);
+$stmt->execute();
 
-  $result = $stmt->get_result();
+$result = $stmt->get_result();
 
-  if(!$result){
-    throw new ApiError(null, 500, 'Error getting week\'s schedule');
+if(!$result){
+  throw new ApiError(null, 500, 'Error getting week\'s schedule');
+}
+
+$data = [];
+$previousEndTime = '';
+$previousStatus = '';
+$previousDate = '';
+$currentDataRow = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+  if ($row['date'] !== $previousDate || $row['start_time'] !== $previousEndTime) {
+    $data[] = $currentDataRow;
+    $currentDataRow = $row;
+    $currentDataRow['componentIDs'] = [intVal($row['roundID'])];
+    $currentDataRow['startingRoundId'] = intVal($row['roundID']);
+    $currentDataRow['roundCount'] = 1;
+  } else {
+    $currentDataRow['roundCount']++;
+    $currentDataRow['componentIDs'][] = [intVal($row['roundID'])];
+    $currentDataRow['end_time'] = $row['end_time'];
   }
+  $previousEndTime = $row['end_time'];
+  $previousStatus = $row['status'];
+  $previousDate = $row['date'];
+}
+$data[] = $currentDataRow;
 
-  $data = [];
-  $previousEndTime = '';
-  $previousStatus = '';
-  $previousDate = '';
-  $currentDataRow = [];
-
-  while ($row = mysqli_fetch_assoc($result)) {
-    if ($row['date'] !== $previousDate || $row['start_time'] !== $previousEndTime) {
-      $data[] = $currentDataRow;
-      $currentDataRow = $row;
-      $currentDataRow['componentIDs'] = [intVal($row['roundID'])];
-      $currentDataRow['startingRoundId'] = intVal($row['roundID']);
-      $currentDataRow['roundCount'] = 1;
-    } else {
-      $currentDataRow['roundCount']++;
-      $currentDataRow['componentIDs'][] = [intVal($row['roundID'])];
-      $currentDataRow['end_time'] = $row['end_time'];
-    }
-    $previousEndTime = $row['end_time'];
-    $previousStatus = $row['status'];
-    $previousDate = $row['date'];
-  }
-  $data[] = $currentDataRow;
-
-  send($data);
+send($data);
