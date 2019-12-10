1e00c10e65c32bde418200c605869df94fa434b7
Tim Horist <TimelikeClosure@users.noreply.github.com>
2019-12-09 20:13:49 -0800

    update shifts-month.php add query error handling

--- server/public/api/shifts-month.php
+++ server/public/api/shifts-month.php
@@ -22,19 +22,26 @@ if (!empty($_GET['endDate'])) {
   $endDate = $_GET['endDate'];
 }
 
-$stmt = $mysqli->prepare("SELECT *
+$statement = $mysqli->prepare("SELECT *
   FROM `round`
   WHERE `user_id`= ?
   AND (`date` >= ? AND `date` <= ?)
   AND (`status` = 'scheduled' OR `status` = 'posted')
   ORDER BY `date` ASC");
+if ($statement == FALSE){
+  throw new ApiError(null, 500, 'Error preparing query');
+}
 
-$stmt->bind_param('iss', $user['userId'], $startDate, $endDate);
-$stmt->execute();
+if (!$statement->bind_param('iss', $user['userId'], $startDate, $endDate)){
+  throw new ApiError(null, 500, 'Error binding query params');
+}
 
-$result = $stmt->get_result();
+if (!$statement->execute()){
+  throw new ApiError(null, 500, 'Error executing query');
+}
 
-if (!$result) {
+$result = $statement->get_result();
+if ($result === FALSE) {
   throw new ApiError(null, 500, 'Error getting month schedule');
 }
 
