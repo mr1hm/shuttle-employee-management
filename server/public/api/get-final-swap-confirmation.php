eaea19dcd3c22ecb84b0ce67267104ee2351c4a0
Tim Horist <TimelikeClosure@users.noreply.github.com>
2019-12-09 19:16:58 -0800

    update get-final-swap-confirmation.php switch second query to prepared statement

--- server/public/api/get-final-swap-confirmation.php
+++ server/public/api/get-final-swap-confirmation.php
@@ -46,25 +46,41 @@ while($row = $result->fetch_assoc()) {
   $output[] = $row;
 }
 
-$query2 = "SELECT t.`id`, t.`user_id`, t.`target_user_id`, t.`date` as request_date, t.`type`, t.`comment`, t.`status`,
-          r.`user_id`, r.`date` as shift_date, r.`start_time`, r.`end_time`, r.`bus_info_id`, r.`id` as round_id,
-          (
-            SELECT `line_name`
-            FROM `route`
-            WHERE `id` = r.`bus_info_id`
-          ) as line_name
-          FROM `transaction` as t
-          JOIN `round` as r
-          ON  t.`target_round_id` = r.`id`
-          WHERE `target_user_id` = $user[userId] AND t.`status` = 'pending' AND t.`type` = 'swap-confirm'";
+$query = "SELECT
+  t.`id`, t.`user_id`, t.`target_user_id`, t.`date` as request_date, t.`type`, t.`comment`, t.`status`,
+  r.`user_id`, r.`date` as shift_date, r.`start_time`, r.`end_time`, r.`bus_info_id`, r.`id` as round_id,
+  (
+    SELECT `line_name`
+    FROM `route`
+    WHERE `id` = r.`bus_info_id`
+  ) as line_name
+  FROM `transaction` as t
+  JOIN `round` as r
+  ON  t.`target_round_id` = r.`id`
+  WHERE `target_user_id` = ?
+  AND t.`status` = 'pending'
+  AND t.`type` = 'swap-confirm'";
 
-if($result2 = $mysqli->query($query2)) {
-  while ($newRow = $result2->fetch_assoc()) {
-    $output[] = $newRow;
-  }
-
-  send($output);
-  exit;
+$statement = $mysqli->prepare($query);
+if ($statement == FALSE){
+  throw new ApiError(null, 500, 'Error preparing query');
 }
 
-throw new ApiError(null, 500, 'Error retrieving final swap confirmations');
+if (!$statement->bind_param('i', $user['userId'])){
+  throw new ApiError(null, 500, 'Error binding query params');
+}
+
+if (!$statement->execute()){
+  throw new ApiError(null, 500, 'Error executing query');
+}
+
+$result = $statement->get_result();
+if ($result == FALSE){
+  throw new ApiError(null, 500, 'Error retrieving final swap confirmations');
+}
+
+$output = [];
+while($row = $result->fetch_assoc()) {
+  $output[] = $row;
+}
+send($output);
