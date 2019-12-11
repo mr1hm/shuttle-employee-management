e03cd24b072abdf971b7a9ee871acae47c4e13e0
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-11 02:44:50 +0000

    Added nav above all routes

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -20,6 +20,7 @@ import TradeNotification from './trade-notification';
 import AdminUserSummary from './admin-user-summary';
 import LiveFieldStatus from './admin-lines-buses-liveFieldStatus';
 import MasterSchedule from './admin-lines-buses-master-schedule';
+import Nav from './nav';
 import NotFound from './errors/not_found';
 
 class App extends React.Component {
@@ -57,39 +58,42 @@ class App extends React.Component {
   render() {
 
     return (
-      <Switch>
-        <Route exact path={['/', '/login']} component={onAuthRedirect(Login, '/welcome')} />
-        <Route path="/admin-day" component={auth.all(AdminShiftsDay, {
-          defaultDate: this.state.presetDateForTesting
-        })} />
-        <Route path="/admin-routes" component={auth.all(AdminRoutes, {
-          defaultDate: this.state.presetDateForTesting
-        })} />
-        <Route path="/admin-operator-availability" component={auth.all(AdminOperatorAvailability)} />
-        <Route path="/admin-user-summary" component={auth.all(AdminUserSummary)} />
-        <Route path="/welcome" component={auth.any(Welcome, {}, '/login')} />
-        <Route path="/live-field-status" component={auth.operations(LiveFieldStatus, {}, '/welcome')} />
-        <Route path="/master-schedule" component={auth.all(MasterSchedule)} />
-        <Route path="/my-info" component={auth.all(MyInfo)} />
-        <Route path="/operator-availability" component={auth.all(OperatorAvailability)} />
-        <Route path="/shifts/available/:date" component={auth.all(ShiftsAvailable)} />
-        <Route path="/shifts/day/shifts-day/:date" component={auth.all(ShiftsDay)} />
-        <Route path="/shifts/details" component={auth.all(ShiftsDetails, {
-          date: this.state.date,
-          shiftId: this.state.shiftId,
-          queryString: this.state.queryString,
-          startSwapTradeTransaction: this.startSwapTradeTransaction
-        })} />
-        <Route path="/shifts/month/shifts-month/:date" component={auth.all(ShiftsMonth)} />
-        <Route path="/shifts/week/shifts-week/:date" component={auth.all(ShiftsWeek)} />
-        <Route path="/trade-notifications" component={auth.all(TradeNotification, {
-          shiftDetails: this.state.shiftDetails
-        })} />
-        <Route path="/trade-swap" component={auth.all(TradeSwap, {
-          shiftDetails: this.state.shiftDetails
-        })} />
-        <Route component={NotFound} />
-      </Switch>
+      <>
+        <Nav/>
+        <Switch>
+          <Route exact path={['/', '/login']} component={onAuthRedirect(Login, '/welcome')} />
+          <Route path="/admin-day" component={auth.all(AdminShiftsDay, {
+            defaultDate: this.state.presetDateForTesting
+          })} />
+          <Route path="/admin-routes" component={auth.all(AdminRoutes, {
+            defaultDate: this.state.presetDateForTesting
+          })} />
+          <Route path="/admin-operator-availability" component={auth.all(AdminOperatorAvailability)} />
+          <Route path="/admin-user-summary" component={auth.all(AdminUserSummary)} />
+          <Route path="/welcome" component={auth.any(Welcome, {}, '/login')} />
+          <Route path="/live-field-status" component={auth.operations(LiveFieldStatus, {}, '/welcome')} />
+          <Route path="/master-schedule" component={auth.all(MasterSchedule)} />
+          <Route path="/my-info" component={auth.all(MyInfo)} />
+          <Route path="/operator-availability" component={auth.all(OperatorAvailability)} />
+          <Route path="/shifts/available/:date" component={auth.all(ShiftsAvailable)} />
+          <Route path="/shifts/day/:date" component={auth.all(ShiftsDay)} />
+          <Route path="/shifts/details" component={auth.all(ShiftsDetails, {
+            date: this.state.date,
+            shiftId: this.state.shiftId,
+            queryString: this.state.queryString,
+            startSwapTradeTransaction: this.startSwapTradeTransaction
+          })} />
+          <Route path="/shifts/month/:date" component={auth.all(ShiftsMonth)} />
+          <Route path="/shifts/week/:date" component={auth.all(ShiftsWeek)} />
+          <Route path="/trade-notifications" component={auth.all(TradeNotification, {
+            shiftDetails: this.state.shiftDetails
+          })} />
+          <Route path="/trade-swap" component={auth.all(TradeSwap, {
+            shiftDetails: this.state.shiftDetails
+          })} />
+          <Route component={NotFound} />
+        </Switch>
+      </>
     );
   }
 }
