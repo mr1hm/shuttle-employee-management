888eabc785d0aa67965185d5fc1045fc0865d7fe
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-12 13:13:30 -0800

    Updated query to join cellProvider table

--- server/public/api/admin-user-summary.php
+++ server/public/api/admin-user-summary.php
@@ -2,10 +2,11 @@
 
 require_once(__DIR__.'/../../lib/startup.php');
 
-$usersQuery = 'SELECT u.id, uci_net_id, last_name, first_name, status, special_route_ok, phone, email, cell_provider
+$usersQuery = 'SELECT u.id, uci_net_id, last_name, first_name, status, special_route_ok, phone, email, cp.cell_provider
 	FROM user AS u
   LEFT JOIN user_roles AS ur ON u.id=ur.user_id
   LEFT JOIN roles AS r ON ur.role_id=r.id
+  LEFT JOIN cellProvider AS cp ON u.cell_provider_id=cp.id
   WHERE r.mid != "super_admin" OR ur.role_id IS NULL
   GROUP BY u.id, uci_net_id, last_name, first_name, status, special_route_ok, phone, email, cell_provider
   ORDER BY last_name ASC';
