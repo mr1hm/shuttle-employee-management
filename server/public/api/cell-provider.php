f360d355ab9424f6855f8fd226c40cf8ee55ba0b
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-12 13:14:23 -0800

    Updated endpoint to send back cell providers with associated table id and a map of cell providers to their id

--- server/public/api/cell-provider.php
+++ server/public/api/cell-provider.php
@@ -2,16 +2,20 @@
 
 require_once(__DIR__.'/../../lib/startup.php');
 
-$query = "SELECT cell_provider FROM cellProvider";
+$query = "SELECT id, cell_provider AS `name` FROM cellProvider";
 
 $result = $mysqli->query($query);
 if (!$result) {
   throw new ApiError(null, 500, 'Error retrieving cell provider data');
 }
 
-$data = [];
+$data = [
+  'providers' => [],
+  'map' => []
+];
 while ($row = $result->fetch_assoc()) {
-  $data[] = $row;
+  $data['providers'][] = $row;
+  $data['map'][$row['id']] = $row['name'];
 }
 
 send($data);
