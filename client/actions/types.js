03e4452675862cc786ecac579ac54826c0b66f8d
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-12 13:10:54 -0800

    Added type for getting cell phone providers

--- client/actions/types.js
+++ client/actions/types.js
@@ -1,6 +1,7 @@
 export default {
   ADMIN_GET_USER_DATA: 'admin_get_user_data',
   ADMIN_UPDATE_USER_ROLE: 'admin_update_user_role',
+  GET_CELL_PROVIDERS: 'get_cell_providers',
   GET_USER_PROFILE: 'get_user_profile',
   GET_USER_PROFILE_ERROR: 'get_user_profile_error',
   TEMP_LOGIN: 'temp_login',
