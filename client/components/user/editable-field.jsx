70d9472a875e5141ed4aade21a8afb2e03ebdc24
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-12 13:16:05 -0800

    Updated to allow for select elements

--- client/components/user/editable-field.jsx
+++ client/components/user/editable-field.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const EditableField = ({ edit = false, onChange, type = 'text', value }) => {
+const EditableField = ({ className, displayValue, edit = false, name, onChange, options, type = 'text', value }) => {
   if (edit) {
+    if (type === 'select') {
+      console.log('Is Array:', Array.isArray(options));
+      return (
+        <select className={className} name={name} value={value || 'default'} onChange={onChange}>
+          <option value="default" disabled>Select an Option</option>
+          {options}
+        </select>
+      );
+    }
+
     return (
-      <div>{value}</div>
+      <input className={className} name={name} onChange={onChange} type={type} value={value} />
     );
   }
 
   return (
-    <input type={type} onChange={onChange} value={value} />
+    <div className={className}>{displayValue || value || <span className="text-danger">Not Set</span>}</div>
   );
 };
 
 EditableField.propTypes = {
+  className: PropTypes.string,
+  displayValue: PropTypes.string,
   edit: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  type: PropTypes.oneOf(['date', 'email', 'number', 'password', 'tel', 'text']).isRequired
+  options: PropTypes.array,
+  type: PropTypes.oneOf(['date', 'email', 'number', 'password', 'select', 'tel', 'text']).isRequired
 };
 
 EditableField.defaultProps = {
+  className: '',
+  displayValue: '',
   edit: false,
   type: 'text'
 };
