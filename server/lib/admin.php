812fb4eae6b26a8718482a66115701447da25188
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-12 13:12:52 -0800

    Updated get user query to join cellProvider table

--- server/lib/admin.php
+++ server/lib/admin.php
@@ -33,12 +33,13 @@ function adminGetUser($uciId) {
   global $mysqli;
 
   $stmt = $mysqli->prepare("SELECT 
-    uci_net_id AS uciNetId, last_name AS lastName, first_name AS firstName, nickname, status, special_route_ok AS specialRouteOk, phone, email, cell_provider AS cellProvider, shirt_size AS shirtSize, CONCAT('[', GROUP_CONCAT('\"', r.mid, '\"'), ']') AS roles
+    uci_net_id AS uciNetId, last_name AS lastName, first_name AS firstName, nickname, status, special_route_ok AS specialRouteOk, phone, email, cp.id AS cellProvider, cp.cell_provider AS cellProviderName, shirt_size AS shirtSize, CONCAT('[', GROUP_CONCAT('\"', r.mid, '\"'), ']') AS roles
     FROM user AS u
     LEFT JOIN user_roles as ur
     ON u.id=ur.user_id
     LEFT JOIN roles AS r
     ON ur.role_id=r.id
+    LEFT JOIN cellProvider AS cp ON u.cell_provider_id=cp.id
     WHERE uci_net_id=?
     GROUP BY uciNetId, lastName, firstName, nickname, status, specialRouteOk, phone, email, cellProvider, shirtSize
   ");
