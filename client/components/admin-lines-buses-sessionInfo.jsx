ec4d7a8eab1b8752499a510a8388c6132701d42e
mr1hm <kevinihm@gmail.com>
2019-12-13 15:36:16 -0800

    added variables to get new minimum hours data

--- client/components/admin-lines-buses-sessionInfo.jsx
+++ client/components/admin-lines-buses-sessionInfo.jsx
@@ -36,7 +36,10 @@ export default class SessionInfo extends React.Component {
     let sessionNotes = null;
     let sessionStart = null;
     let sessionEnd = null;
-    let minHoursReq = null;
+    let minOperatorHours = null;
+    let minOperationsHours = null;
+    let minTrainerHours = null;
+    let minTraineeHours = null;
     let sessionID = null;
     if (this.props.currentSession === 'All Sessions') return <span>Please select a session to see details</span>;
     if (sessionInfo.length === 0) return <span>There are no lines and buses</span>;
@@ -45,7 +48,10 @@ export default class SessionInfo extends React.Component {
       sessionName = line.sessionName;
       sessionStart = line.sessionStart;
       sessionEnd = line.sessionEnd;
-      minHoursReq = line.minHoursReq;
+      minOperatorHours = line.min_operator_hours;
+      minOperationsHours = line.min_operations_hours;
+      minTrainerHours = line.min_trainer_hours;
+      minTraineeHours = line.min_trainee_hours;
       if (line.sessionNotes) sessionNotes = line.sessionNotes;
       if (line.sessionHolidays) sessionHolidays = line.sessionHolidays;
       if (line.activeBuses.length > 0) {
@@ -70,7 +76,15 @@ export default class SessionInfo extends React.Component {
       <br/>
         <span className="sessionInfoTitle">Session Notes: </span><span className="sessionInfoValues">{sessionNotes || 'None'}</span>
       <br/>
-        <span className="sessionInfoTitle">Session Minimum Hours Required: </span><span className="sessionInfoValues">{minHoursReq}</span>
+        <span className="sessionInfoTitle">Session Minimum Hours Required:
+          <br />
+          <ul>
+            <li>Operator: {minOperatorHours}</li>
+            <li>Operations: {minOperationsHours}</li>
+            <li>Trainer: {minTrainerHours}</li>
+            <li>Trainee: {minTraineeHours}</li>
+          </ul>
+        </span>
       </>
     );
   }
